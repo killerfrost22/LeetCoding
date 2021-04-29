42976048e33f0af6c95c2f237a56bc142cf7d89e
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-04-29 08:32:13 -0400

    ss

---
+++ codes/7. Reverse Integer/codes.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = [1,-1][x<0]
+        rev, x =0, abs(x)
+        while x:
+            x, mod = divmod(x,10)
+            rev = rev*10 + mod
+        return sign*rev if -pow(2,31)<=sign*rev <=pow(2,31)-1 else 0
