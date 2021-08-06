19e9e62452b50e3fa7a0f6cd20f4b8cae731575a
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-08-06 15:23:00 -0400

    14

---
+++ codes/14. Longest Common Prefix/code.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        if len(strs) == 1:
+            return strs[0]
+        min_len = min([len(s) for s in strs])
+        if min_len == 0:
+            return ''
+        for i in range(min_len):
+            for j in range(len(strs) - 1):
+                if strs[j][i] != strs[][j + 1][i]:
+                    return strs[0][:i]
+        return strs[0][:i + 1]
+
+
+# Path: codes\14. Longest Common Prefix\code.py
+
+            
