9577775b2374b029d4e0630ca4145a4e0237437e
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-08-09 12:32:01 -0400

    ds

--- codes/20. Valid Parentheses/Solution#2.py
+++ codes/20. Valid Parentheses/Solution#2.py
@@ -9,7 +9,7 @@ class Solution(object):
         dict = {"]": "[", "}": "{", ")": "("}
         for char in s:
             if char in dict:
-                if stack and  stack[-1] == dict[char]:
+                if stack and  stack[-1] == dict[char]: ## stack[-1] is the last value in the stack  
                     stack.pop()
                 else:
                     return False
@@ -17,3 +17,14 @@ class Solution(object):
                 stack.append(char)
             
         return True if not stack else False
+
+
+# create a list of prime numbers
+# prime_numbers = [2, 3, 5, 7]
+
+# # remove the element at index 2
+# removed_element = prime_numbers.pop(2)
+
+# print('Removed Element:', removed_element)
+
+# # Output: Removed Element: 5
