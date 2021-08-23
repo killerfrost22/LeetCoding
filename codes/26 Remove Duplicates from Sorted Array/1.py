1664b2e4f6b666f7024ce63b8adc890a098ac7eb
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-08-22 22:36:24 -0400

    26

--- codes/26 Remove Duplicates from Sorted Array/1.py
+++ codes/26 Remove Duplicates from Sorted Array/1.py
@@ -4,4 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        if not nums:
+            return 0
+        res = 0
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                res +=1
+                nums[res] = nums[i]
+        return res+1
