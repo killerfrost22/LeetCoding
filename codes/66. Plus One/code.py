9db5bd2d5164f95ecf2c8fd17c2ac14cdc460725
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-11-04 20:44:28 -0400

    Update code.py

---
+++ codes/66. Plus One/code.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if len(digits) == 1 and digits[0] == 9:
+            return [1,0]
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+        else:
+            digits = self.plusOne(digits[:-1])
+            digits.append(0)
+            return digits
+            
